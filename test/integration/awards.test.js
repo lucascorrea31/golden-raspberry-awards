ec62e5372b4a83281e76590d70a4f37c0cd35f08
Lucas Correa <lucas.nhl@gmail.com>
2024-11-13 16:42:59 -0300

    add a test to validate the min and max intervals

--- test/integration/awards.test.js
+++ test/integration/awards.test.js
@@ -24,4 +24,24 @@ describe("GET /awards/producers/intervals", () => {
 		expect(response.body).toHaveProperty("min");
 		expect(response.body).toHaveProperty("max");
 	});
+
+	// Este teste depende exclusivamente da massa de dados disponibilizada.
+	// Ao alterar os dados, estes testes tendem a falhar.
+	it("should return a producer with a min interval equals 1", async () => {
+		const response = await request(app).get("/awards/producers/intervals");
+		expect(response.statusCode).toBe(200);
+		expect(response.body).toHaveProperty("min");
+		expect(response.body.min.length).toEqual(1);
+		expect(response.body.min[0]).toHaveProperty("interval");
+		expect(response.body.min[0].interval).toEqual(1);
+	});
+
+	it("should return a producer with a max interval equals 13", async () => {
+		const response = await request(app).get("/awards/producers/intervals");
+		expect(response.statusCode).toBe(200);
+		expect(response.body).toHaveProperty("max");
+		expect(response.body.max.length).toEqual(1);
+		expect(response.body.max[0]).toHaveProperty("interval");
+		expect(response.body.max[0].interval).toEqual(13);
+	});
 });
